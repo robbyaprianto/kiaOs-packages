d710fb028c7f7d78388b77e977d34db6e0fc57b6
robbyaprianto <robbyaprianto@hotmail.com>
2022-03-09 08:31:11 +0700

    add opemclash & edit tinyfm path

--- luci-app-tinyfm/luasrc/controller/tinyfm.lua
+++ luci-app-tinyfm/luasrc/controller/tinyfm.lua
@@ -1,5 +1,4 @@
 module("luci.controller.tinyfm", package.seeall)
 function index()
-	entry({"admin", "nas"}, firstchild(), "NAS", 44).dependent=false
-	entry({"admin", "nas", "tinyfm"}, template("tinyfm"), _("Tiny File Manager"), 55).dependent=true
+	entry({"admin", "services", "tinyfm"}, template("tinyfm"), _("File Manager"), 55).dependent=true
 end
